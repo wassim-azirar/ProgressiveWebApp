046e0574ad2ac9488a45f195e337b0d51afaa2b9
Wassim AZIRAR <wassim.azirar@gmail.com>
2018-03-11 15:03:06 +0100

    Added @Angular/Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,37 @@
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatInputModule,
+  MatSelectModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatCardModule
+} from '@angular/material';
+import { DataService } from './services/data.service';
+import { GeolocationService } from './services/geolocation.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { GeolocationService } from './services/geolocation.service';
-import { DataService } from './services/data.service';
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatCardModule,
+    MatSlideToggleModule
+  ],
   providers: [GeolocationService, DataService],
   bootstrap: [AppComponent]
 })
