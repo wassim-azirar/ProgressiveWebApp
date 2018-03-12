31a34bb1638f7972ec8ea2f57906ce33ac0cdd86
Wassim AZIRAR <wassim.azirar@gmail.com>
2018-03-13 00:59:54 +0100

    Added map and sharing

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { Coffee } from '../../logic/coffee';
 import { Router } from '@angular/router';
+import { GeolocationService } from '../../services/geolocation.service';
 
 @Component({
   selector: 'app-list',
@@ -11,7 +12,11 @@ import { Router } from '@angular/router';
 export class ListComponent implements OnInit {
   public coffeeList: Array<Coffee>;
 
-  constructor(private dataService: DataService, private router: Router) {}
+  constructor(
+    private dataService: DataService,
+    private router: Router,
+    private geolocation: GeolocationService
+  ) {}
 
   ngOnInit() {
     this.dataService.getList(list => {
@@ -21,5 +26,33 @@ export class ListComponent implements OnInit {
 
   openDetails(coffee: Coffee) {
     this.router.navigate(['/coffee', coffee._id]);
+  }
+
+  goMap(coffee: Coffee) {
+    const mapUrl = this.geolocation.getMapLink(coffee.location);
+    location.href = mapUrl;
+  }
+
+  share(coffee: Coffee) {
+    const shareText = `I had this coffee at ${coffee.place} and for me its a ${
+      coffee.rating
+    } star coffee`;
+
+    if ('share' in navigator) {
+      (navigator as any)
+        .share({
+          title: coffee.name,
+          text: shareText,
+          url: window.location.href
+        })
+        .then(() => {
+          console.log('shared');
+        })
+        .catch(error => console.log('Error sharing', error));
+    } else {
+      console.log('trying to share using Whatsapp');
+      const shareUrl = `whatsapp://send?text=${encodeURIComponent(shareText)}`;
+      location.href = shareUrl;
+    }
   }
 }
