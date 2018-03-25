971fb30287e1213a0cd08eb59c94e0baf5a732ed
Wassim AZIRAR <wassim.azirar@gmail.com>
2018-03-26 01:32:00 +0200

    remove firebase

--- src/environments/firebase.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'your-api-key',
-  authDomain: 'your-website.firebaseapp.com',
-  databaseURL: 'https://your-website.firebaseio.com',
-  projectId: 'your-firebase-project-id',
-  storageBucket: 'your-storage-bucket',
-  messagingSenderId: 'your-messaging-sender-id'
-};
