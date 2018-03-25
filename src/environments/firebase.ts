ed1d34bef2fc0a4406c8deb93014f084cd43b1af
Wassim AZIRAR <wassim.azirar@gmail.com>
2018-03-26 01:12:07 +0200

    Create firebase.ts

---
+++ src/environments/firebase.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'your-api-key',
+  authDomain: 'your-website.firebaseapp.com',
+  databaseURL: 'https://your-website.firebaseio.com',
+  projectId: 'your-firebase-project-id',
+  storageBucket: 'your-storage-bucket',
+  messagingSenderId: 'your-messaging-sender-id'
+};
